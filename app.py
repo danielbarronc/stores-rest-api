062606ec8cbc18a0c3492edff67dee855a539071
Daniel Barron Colin <danielbarronc@gmail.com>
2022-05-26 17:56:18 -0500

    Addes PostgrSQL environment variables and psycopgm2 VERSION 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -10,7 +11,13 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
 app.config['SQLALCHEMT_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
